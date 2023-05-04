9ca795153002b61433974a30683510315c2d60a1
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-04 00:50:36 -0300

    add busca via querystring e por qualquer fuso_horario

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from datetime import datetime, timedelta
 
 app = FastAPI()
 
-@app.get("/horarios/{fuso_horario}")
-async def get_horarios(fuso_horario: str):
-    if fuso_horario == "brasil":
-        tz = pytz.timezone('America/Sao_Paulo')
-    elif fuso_horario == "ny":
-        tz = pytz.timezone('America/New_York')
-    else:
+@app.get("/horarios/")
+async def get_horarios(fuso_horario: str = 'America/Sao_Paulo'):#fuso_horario é um parâmetro de query que será passado na url, poderia usar uma requisição post e passar o fuso horário no corpo da requisição também
+    if fuso_horario not in pytz.all_timezones_set: #usando o set para melhorar a performance no lookup, já que o set é uma estrutura de dados otimizada para lookup
         raise HTTPException(status_code=400, detail="Fuso horário não reconhecido")
-    agora = datetime.now(tz)
-    hora = agora.strftime("%H:%M:%S")
+    else:
+        timeZone = pytz.timezone(fuso_horario)
+        agora = datetime.now(timeZone)
+        hora = agora.strftime("%H:%M:%S")
     return {"horario": hora, "fuso_horario": fuso_horario}
