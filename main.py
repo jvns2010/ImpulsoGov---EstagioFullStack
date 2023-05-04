d2761f7380f61373f98238e8b5c21b82f27a0fdc
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-04 00:01:04 -0300

    implementando get fuso_horario
    A função recebe uma string com o tipo de fuso horario e retorna a hora e o fuso

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+import pytz
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
+@app.get("/horarios/{fuso_horario}")
+async def get_horarios(fuso_horario: str):
+    if fuso_horario == "brasil":
+        tz = pytz.timezone('America/Sao_Paulo')
+    elif fuso_horario == "ny":
+        tz = pytz.timezone('America/New_York')
+    agora = datetime.now(tz)
+    hora = agora.strftime("%H:%M:%S")
+    return {"horario": hora, "fuso_horario": fuso_horario}
