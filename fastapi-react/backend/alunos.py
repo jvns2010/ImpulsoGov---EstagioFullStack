8e9506da481701648b66c7502ad5780cb1de5c34
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-04 19:39:03 -0300

    npx create-react-app + mudanças de estrutura

---
+++ fastapi-react/backend/alunos.py
@@ -0,0 +1,4 @@
+alunos = [{ "nome" : "José Alves Dos Santos", "nota" : 37 },
+{ "nome" : "Anderson Da Silva", "nota" : 49 }, 
+{ "nome" : "Maria Ferreira", "nota" : 68 },
+{ "nome" : "Ana Oliveira", "nota" : 87 }]
--- fastapi-react/backend/app.py
+++ fastapi-react/backend/app.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import pytz
 from datetime import datetime, timedelta
+from alunos import alunos
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+
+@app.get("/", tags=["root"])
+def read_root() -> dict:
+    return {"React and FastAPI": "Hello World! This is the root of the API"}
 
 @app.get("/horarios/")#sao paulo como fuso padrão
 async def get_horarios(fuso_horario: str = 'America/Sao_Paulo'):#fuso_horario é um parâmetro de query que será passado na url, poderia usar uma requisição post e passar o fuso horário no corpo da requisição também
@@ -13,3 +35,7 @@ async def get_horarios(fuso_horario: str = 'America/Sao_Paulo'):#fuso_horario é
         agora = datetime.now(timeZone)
         hora = agora.strftime("%H:%M:%S")
     return {"Horário": hora, "Fuso horário": fuso_horario}
+
+@app.get("/alunos", tags=["alunos"])
+async def get_alunos() -> dict:
+    return { "alunos": alunos }    
