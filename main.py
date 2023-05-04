400fa513ea8b2aebeb83920da27b8277db26c971
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-04 00:58:13 -0300

    Update string de resposta

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from datetime import datetime, timedelta
 
 app = FastAPI()
 
-@app.get("/horarios/")
+@app.get("/horarios/")#sao paulo como fuso padrão
 async def get_horarios(fuso_horario: str = 'America/Sao_Paulo'):#fuso_horario é um parâmetro de query que será passado na url, poderia usar uma requisição post e passar o fuso horário no corpo da requisição também
     if fuso_horario not in pytz.all_timezones_set: #usando o set para melhorar a performance no lookup, já que o set é uma estrutura de dados otimizada para lookup
-        raise HTTPException(status_code=400, detail="Fuso horário não reconhecido")
+        raise HTTPException(status_code=400, detail="Fuso horário não reconhecido") #400 = bad request
     else:
         timeZone = pytz.timezone(fuso_horario)
         agora = datetime.now(timeZone)
         hora = agora.strftime("%H:%M:%S")
-    return {"horario": hora, "fuso_horario": fuso_horario}
+    return {"Horário": hora, "Fuso horário": fuso_horario}
