93c32dae42ce93a57611f84fb7275b20e77b4bb1
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-04 00:01:52 -0300

    implementando exceção de fuso não reconhecido

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import pytz
 from datetime import datetime, timedelta
 
@@ -10,6 +10,8 @@ async def get_horarios(fuso_horario: str):
         tz = pytz.timezone('America/Sao_Paulo')
     elif fuso_horario == "ny":
         tz = pytz.timezone('America/New_York')
+    else:
+        raise HTTPException(status_code=400, detail="Fuso horário não reconhecido")
     agora = datetime.now(tz)
     hora = agora.strftime("%H:%M:%S")
     return {"horario": hora, "fuso_horario": fuso_horario}
