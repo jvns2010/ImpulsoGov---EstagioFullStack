298e8c015773b316df84e4be1bd0e1e6b3f80db2
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-05 18:14:39 -0300

    Adicionando comentários

--- fastapi-react/backend/app.py
+++ fastapi-react/backend/app.py
@@ -28,7 +28,7 @@ def read_root() -> dict:
 
 @app.get("/horarios/")#sao paulo como fuso padrão
 async def get_horarios(fuso_horario: str = 'America/Sao_Paulo'):#fuso_horario é um parâmetro de query que será passado na url, poderia usar uma requisição post e passar o fuso horário no corpo da requisição também
-    if fuso_horario not in pytz.all_timezones_set: #usando o set para melhorar a performance no lookup, já que o set é uma estrutura de dados otimizada para lookup
+    if fuso_horario not in pytz.all_timezones_set: #usando o set para melhorar a performance no lookup #pesquisar sobre lazy sets python
         raise HTTPException(status_code=400, detail="Fuso horário não reconhecido") #400 = bad request
     else:
         timeZone = pytz.timezone(fuso_horario)
@@ -36,6 +36,6 @@ async def get_horarios(fuso_horario: str = 'America/Sao_Paulo'):#fuso_horario é
         hora = agora.strftime("%H:%M:%S")
     return {"Horário": hora, "Fuso horário": fuso_horario}
 
-@app.get("/alunos", tags=["alunos"])
+@app.get("/alunos", tags=["alunos"])#rota para retornar os alunos e ser consumida pelo react
 async def get_alunos() -> dict:
     return { "alunos": alunos }    
