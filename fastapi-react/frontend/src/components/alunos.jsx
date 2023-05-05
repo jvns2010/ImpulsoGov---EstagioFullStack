4945e802076f3d9b9ecd6c1e1b28299840017182
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-05 18:14:13 -0300

    Implementando solução consumindo a lista pela fastAPI

--- fastapi-react/frontend/src/components/alunos.jsx
+++ fastapi-react/frontend/src/components/alunos.jsx
@@ -1,10 +1,43 @@
-import React, { useEffect, useState } from "react";
-const ListaAlunos = ({ alunos }) => { // Recebe os alunos como props depois ordena em ordem alfabetica
-  const sortedAlunos = alunos.sort((a, b) => a.nome.localeCompare(b.nome));
+import React, { useState, useEffect } from "react";
+import customError from "../customError";
+function ListaAlunos() {
+  
+  // estado para armazenar a lista de alunos
+  const [alunos, setAlunos] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/alunos");
+        if (!response.ok){
+          //se a resposta não for ok, lançamos uma exceção com a mensagem de erro e o status code
+          //nesse caso status code é arbitrário, podemos definir um outro arquivo com as possíveis mensagens de erro e status code
+          console.log(response)
+          throw new customError(500, "Não foi possível buscar os dados dos alunos.");
+          
+        }
+        const alunosResponse = await response.json();
+        //ordena a lista de alunos em ordem alfabética pelo nome
+        const sortedAlunos = alunosResponse.alunos.sort((a, b) => a.nome.localeCompare(b.nome));
+        setAlunos(sortedAlunos);
+      } catch (error) {
+        if(error instanceof customError){
+          alert(error.message);
+        }else if(error instanceof TypeError){ 
+          //se o erro for de conexão, exibe uma mensagem de erro genérica
+          //TypeError aqui é o tipo de erro que o fetch retorna quando não consegue se conectar ao servidor
+          alert('Não foi possível conectar ao servidor. Tente novamente mais tarde.');
+        }else{
+          alert("Ocorreu um erro inesperado.");
+        }
+      }
+    }
+    fetchData();
+  }, []);
+  console.log(alunos);
   return (
     <ul>
-      {sortedAlunos.map((aluno) => { // para cada aluno verifica a nota pra aplicar o estilo e depois retorna o li com o nome do aluno e estilo aplicado
+      {alunos.map((aluno) => { // para cada aluno verifica a nota pra aplicar o estilo e depois retorna o li com o nome do aluno e estilo aplicado
         const estiloCor = aluno.nota < 50 ? { color: 'red' } : { color: 'green' };
         return (// retorna o li com o nome do aluno, usando o nome como key 
           <li key={aluno.nome} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px', ...estiloCor }}>
