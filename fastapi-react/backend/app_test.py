4e5cc32c8fb2761783dcb8b14baa91a1ee0b41ab
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-05-05 18:38:58 -0300

    Implementando teste  das rotas getHorarios e getAlunos

--- fastapi-react/backend/app_test.py
+++ fastapi-react/backend/app_test.py
@@ -11,3 +11,24 @@ def test_horarios_fuso_invalido():
     assert response.status_code == 400
 
 
+#testa se a rota /horarios retorna um horário válido para um fuso horário conhecido
+#nesse caso o fuso padrao de sp
+def test_horarios():
+    response = requests.get('http://localhost:8000/horarios/?fuso_horario=America/Sao_Paulo')
+    assert response.status_code == 200
+    result = json.loads(response.content)
+    ## checa pelas strings horario e fuso horario na resposta
+    assert "Horário" in result
+    assert "Fuso horário" in result
+    assert result["Horário"] != "" ## checa se o horario nao esta vazio
+
+
+# testa se a rota /alunos retorna uma lista ou set ou dict com os alunos e se o tamanho é maior que 0
+## lembrete talvez exista uma forma mais elegante de fazer esse teste
+def test_alunos():
+    response = requests.get('http://localhost:8000/alunos')
+    assert response.status_code == 200
+    result = json.loads(response.content)
+    assert "alunos" in result
+    assert isinstance(result["alunos"], (list, set, dict))
+    assert len(result["alunos"]) > 0
