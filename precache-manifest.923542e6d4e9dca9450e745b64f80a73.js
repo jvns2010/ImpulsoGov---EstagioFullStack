65907b4408a0a10fe0294ee1db586dd20fa74d89
João Vittor Souza <80353966+jvns2010@users.noreply.github.com>
2023-05-07 22:08:23 -0300

    Updates

---
+++ precache-manifest.923542e6d4e9dca9450e745b64f80a73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "57079b3815ce2a8333d0",
+    "url": "/ImpulsoGov---EstagioFullStack/static/js/main.b3ded4ae.chunk.js"
+  },
+  {
+    "revision": "5f2893d25937e08e4d82",
+    "url": "/ImpulsoGov---EstagioFullStack/static/js/runtime~main.3064190f.js"
+  },
+  {
+    "revision": "8790e9cd3af12826e225",
+    "url": "/ImpulsoGov---EstagioFullStack/static/js/2.c3941040.chunk.js"
+  },
+  {
+    "revision": "2a8d9507c5d8a00279e1bf9471d2e16d",
+    "url": "/ImpulsoGov---EstagioFullStack/index.html"
+  }
+];
