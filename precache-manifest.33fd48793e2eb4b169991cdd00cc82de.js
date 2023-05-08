7351478551db679599d95bf1621c25bf611e6bbe
João Vittor Souza <80353966+jvns2010@users.noreply.github.com>
2023-05-07 22:11:24 -0300

    Updates

---
+++ precache-manifest.33fd48793e2eb4b169991cdd00cc82de.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "57079b3815ce2a8333d0",
+    "url": "/static/js/main.b3ded4ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8790e9cd3af12826e225",
+    "url": "/static/js/2.c3941040.chunk.js"
+  },
+  {
+    "revision": "08fc13454cb65a48491703827fe3637e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ImpulsoGov---EstagioFullStack/precache-manifest.923542e6d4e9dca9450e745b64f80a73.js"
+  "/precache-manifest.33fd48793e2eb4b169991cdd00cc82de.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ImpulsoGov---EstagioFullStack/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
