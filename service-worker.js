6156733186025817fb45a9bbc6cdb3a630ade384
João Vittor Souza <80353966+jvns2010@users.noreply.github.com>
2023-05-07 22:17:10 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33fd48793e2eb4b169991cdd00cc82de.js"
+  "/ImpulsoGov---EstagioFullStack/precache-manifest.923542e6d4e9dca9450e745b64f80a73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ImpulsoGov---EstagioFullStack/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
